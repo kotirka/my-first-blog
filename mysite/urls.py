4e2b2ee6c892fbbf449ab969314c64ee6688b5e7
kotirka <kotirka.ua@gmail.com>
2015-05-16 13:48:22 +0300

    My Django Girls app, first commit

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+] 
